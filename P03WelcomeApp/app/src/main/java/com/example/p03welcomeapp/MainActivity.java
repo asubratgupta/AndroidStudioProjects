39e3b0cd67dff578ec4c874c76e0263d8fba9e5f
asubratgupta <asubratgupta@.com>
2020-06-17 08:34:01 +0530

    03

---
+++ P03WelcomeApp/app/src/main/java/com/example/p03welcomeapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.p03welcomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Welcome "+editText.getText().toString()+"!!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
